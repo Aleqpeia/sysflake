07f4580807166c636097a84527d75914a8a86d71
efyis <mbobylyov@biph.kiev.ua>
2026-01-09 01:07:40 +0200

    astronvim

--- flake/nvim/neovim-overlay.nix
+++ flake/nvim/neovim-overlay.nix
@@ -1,159 +1,52 @@
-# This overlay, when applied to nixpkgs, adds the final neovim derivation to nixpkgs.
+# Neovim overlay for AstroNvim
+# This provides a minimal neovim with LSPs and tools, letting AstroNvim manage plugins
 {inputs}: final: prev:
 with final.pkgs.lib; let
   pkgs = final;
-
-  # Use this to create a plugin from a flake input
-  mkNvimPlugin = src: pname:
-    pkgs.vimUtils.buildVimPlugin {
-      inherit pname src;
-      version = src.lastModifiedDate;
-    };
-
-  # Make sure we use the pinned nixpkgs instance for wrapNeovimUnstable,
-  # otherwise it could have an incompatible signature when applying this overlay.
-  pkgs-locked = inputs.nixpkgs.legacyPackages.${pkgs.system};
-
-  # This is the helper function that builds the Neovim derivation.
-  mkNeovim = pkgs.callPackage ./mkNeovim.nix {
-      inherit (pkgs-locked) wrapNeovimUnstable neovimUtils;
-  };
-
-  # A plugin can either be a package or an attrset, such as
-  # { plugin = <plugin>; # the package, e.g. pkgs.vimPlugins.nim-cmp
-  #   config = <config>; # String; a config that will be loaded with the plugin
-  #   # Boolean; Whether to automatically load the plugin as a 'start' plugin,
-  #   # or as an 'opt' plugin, that can be loaded with `:packadd!`
-  #   optional = <true|false>; # Default: false
-  #   ...
-  # }
-  all-plugins = with pkgs.vimPlugins; [
-    # plugins from nixpkgs go in here.
-    # https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query=vimPlugins
-    nvim-treesitter.withAllGrammars
-    luasnip # snippets | https://github.com/l3mon4d3/luasnip/
-    # plugin and lsp management
-    lazy-nvim # https://github.com/folke/lazy.nvim
-    mason-nvim # https://github.com/williamboman/mason.nvim
-    mason-lspconfig # https://github.com/williamboman/mason-lspconfig.nvim
-    mason-nvim-dap # https://github.com/jay-babu/mason-nvim-dap.nvim
-    # ^ plugin and lsp management
-    # nvim-cmp (autocompletion) and extensions
-    nvim-cmp # https://github.com/hrsh7th/nvim-cmp
-    cmp_luasnip # snippets autocompletion extension for nim-cmp | https://github.com/saadparwaiz1/cmp_luasnip/
-    lspkind-nvim # vscode-like LSP pictograms | https://github.com/onsails/lspkind.nvim/
-    cmp-nvim-lsp # LSP as completion source | https://github.com/hrsh7th/cmp-nvim-lsp/
-    cmp-nvim-lsp-signature-help # https://github.com/hrsh7th/cmp-nvim-lsp-signature-help/
-    cmp-buffer # current buffer as completion source | https://github.com/hrsh7th/cmp-buffer/
-    cmp-path # file paths as completion source | https://github.com/hrsh7th/cmp-path/
-    cmp-nvim-lua # neovim lua API as completion source | https://github.com/hrsh7th/cmp-nvim-lua/
-    cmp-cmdline # cmp command line suggestions
-    cmp-cmdline-history # cmp command line history suggestions
-    # ^ nvim-cmp extensions
-    # git integration plugins
-    diffview-nvim # https://github.com/sindrets/diffview.nvim/
-    neogit # https://github.com/TimUntersberger/neogit/
-    gitsigns-nvim # https://github.com/lewis6991/gitsigns.nvim/
-    vim-fugitive # https://github.com/tpope/vim-fugitive/
-    # ^ git integration plugins
-    # telescope and extensions
-    telescope-nvim # https://github.com/nvim-telescope/telescope.nvim/
-    telescope-fzy-native-nvim # https://github.com/nvim-telescope/telescope-fzy-native.nvim
-    # telescope-smart-history-nvim # https://github.com/nvim-telescope/telescope-smart-history.nvim
-    # ^ telescope and extensions
-    # UI
-    lualine-nvim # Status line | https://github.com/nvim-lualine/lualine.nvim/
-    nvim-navic # Add LSP location to lualine | https://github.com/SmiteshP/nvim-navic
-    statuscol-nvim # Status column | https://github.com/luukvbaal/statuscol.nvim/
-    nvim-treesitter-context # nvim-treesitter-context
-    # ^ UI
-    # language support
-    # ^ language support
-    # navigation/editing enhancement plugins
-    vim-unimpaired # predefined ] and [ navigation keymaps | https://github.com/tpope/vim-unimpaired/
-    eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
-    nvim-surround # https://github.com/kylechui/nvim-surround/
-    nvim-treesitter-textobjects # https://github.com/nvim-treesitter/nvim-treesitter-textobjects/
-    nvim-ts-context-commentstring # https://github.com/joosepalviste/nvim-ts-context-commentstring/
-    nvim-cursorline # https://github.com/yamatsum/nvim-cursorline
-    nvim-neotree # https://github.com/nvim-neo-tree/neo-tree.nvim
-    # ^ navigation/editing enhancement plugins
-    # Useful utilities
-    nvim-unception # Prevent nested neovim sessions | nvim-unception
-    nvim-mini-animate # https://github.com/echasnovski/mini.animate
-      # ^ Useful utilities
-    # libraries that other plugins depend on
-    sqlite-lua
-    plenary-nvim
-    nvim-web-devicons
-    vim-repeat
-    # ^ libraries that other plugins depend on
-    # bleeding-edge plugins from flake inputs
-    # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
-    # ^ bleeding-edge plugins from flake inputs
-    which-key-nvim
-    # themes 
-    kanagawa-nvim # kanagawa
-    catppuccin-nvim # catppuccin
-    nord-nvim # nord
-    everforest-nvim # everforest
-    # ^ themes
-    # ai 
-    copilot-lua
-    copilot-cmp
-    # ^ ai
-  ];
-
-  extraPackages = with pkgs; [
-    # language servers, etc.
-    lua-language-server
-    nil # nix LSP
-    nixd # nix LSP (alternative)
-    stylua # lua formatter
-    ruff # python linter
-    pyright # python LSP
-    rust-analyzer # rust LSP
-    shfmt # shell formatter
-    nixfmt-rfc-style # nix formatter
-  ];
 in {
-  # This is the neovim derivation
-  # returned by the overlay
-  nvim-pkg = mkNeovim {
-    plugins = all-plugins;
-    inherit extraPackages;
-  };
+  # Neovim with AstroNvim-compatible setup
+  nvim-pkg = pkgs.neovim-unwrapped.overrideAttrs (old: {
+    propagatedBuildInputs = (old.propagatedBuildInputs or []) ++ (with pkgs; [
+      # Language servers
+      lua-language-server
+      nil                    # Nix LSP
+      nixd                   # Nix LSP (alternative)
+      pyright                # Python LSP
+      ruff                   # Python linter/formatter
+      rust-analyzer          # Rust LSP
+      typescript-language-server  # TypeScript/JavaScript LSP
+      vscode-langservers-extracted  # HTML/CSS/JSON/ESLint LSPs
+      yaml-language-server   # YAML LSP
+      marksman               # Markdown LSP
+      
+      # Formatters
+      stylua                 # Lua
+      black                  # Python
+      isort                  # Python imports
+      prettier               # JS/TS/JSON/YAML/Markdown
+      shfmt                  # Shell
+      nixfmt-rfc-style       # Nix
+      
+      # Linters
+      shellcheck             # Shell
+      
+      # DAP (Debug Adapter Protocol)
+      # vscode-js-debug      # JavaScript/TypeScript debugging
+      
+      # Build tools
+      gcc
+      gnumake
+      cmake
+      
+      # Essential tools
+      git
+      ripgrep
+      fd
+      lazygit
+      tree-sitter
+    ]);
+  });
 
-  # This is meant to be used within a devshell.
-  # Instead of loading the lua Neovim configuration from
-  # the Nix store, it is loaded from $XDG_CONFIG_HOME/nvim-dev
-  nvim-dev = mkNeovim {
-    plugins = all-plugins;
-    inherit extraPackages;
-    appName = "nvim-dev";
-    wrapRc = false;
-  };
-
-  # This can be symlinked in the devShell's shellHook
-  nvim-luarc-json = final.mk-luarc-json or (pkgs.writeText "luarc.json" (builtins.toJSON {
-    runtime.version = "LuaJIT";
-    runtime.path = [ "?.lua" "?/init.lua" ];
-    workspace.library = map (p: "${p}/lua") all-plugins;
-    workspace.checkThirdParty = false;
-  }));
-
-  # You can add as many derivations as you like.
-  # Use `ignoreConfigRegexes` to filter out config
-  # files you would not like to include.
-  #
-  # For example:
-  #
-  # nvim-pkg-no-telescope = mkNeovim {
-  #   plugins = [];
-  #   ignoreConfigRegexes = [
-  #     "^plugin/telescope.lua"
-  #     "^ftplugin/.*.lua"
-  #   ];
-  #   inherit extraPackages;
-  # };
+  # For development/testing
+  nvim-dev = pkgs.nvim-pkg;
 }
--- modules/home/programs/neovim.nix
+++ modules/home/programs/neovim.nix
@@ -1,26 +1,56 @@
-{ pkgs, self, ... }:
+{ config, pkgs, lib, ... }:
 {
-  # Use custom nvim from the flake's packages
-  # This wraps your existing neovim configuration
-  home.packages = [
-    self.packages.${pkgs.system}.khanelivim
-  ]
-  ++ (with pkgs; [
-    # Clipboard support
-    wl-clipboard
-    xclip
-
-    # Tree-sitter CLI for grammar updates
-    tree-sitter
-
-    # Language-specific formatters/tools (supplement what's in nixvim)
-    nodePackages.prettier
-    shfmt
-    stylua
-    nixfmt-rfc-style
-  ]);
-
-  # Ensure nvim is the default editor
+  # Neovim for AstroNvim
+  # AstroNvim will manage its own plugins via Lazy.nvim
+  # This provides neovim + LSPs/tools from nix
+  
+  programs.neovim = {
+    enable = true;
+    package = pkgs.nvim-pkg;
+    
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    
+    withNodeJs = true;
+    withPython3 = true;
+    
+    extraPackages = with pkgs; [
+      # Language servers (for Mason/AstroNvim to use)
+      lua-language-server
+      nil
+      nixd
+      pyright
+      ruff
+      rust-analyzer
+      typescript-language-server
+      vscode-langservers-extracted  # html, css, json, eslint
+      yaml-language-server
+      marksman
+      
+      # Formatters
+      stylua
+      black
+      isort
+      prettier
+      shfmt
+      nixfmt-rfc-style
+      shellcheck
+      
+      # Build tools
+      gcc
+      gnumake
+      cmake
+      
+      # Essential tools
+      ripgrep
+      fd
+      lazygit
+      tree-sitter
+    ];
+  };
+  
+  # Session variables
   home.sessionVariables = {
     EDITOR = "nvim";
     VISUAL = "nvim";
